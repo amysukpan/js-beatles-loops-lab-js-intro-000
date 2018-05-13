99e7744da1d9b33eecb1f3f6a735bd2256a20fbc
amrudees@yahoo.com <amrudees@yahoo.com>
2018-05-13 00:58:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function theBeatlesPlay(musicians,instruments){
+  var beatBand = new Array()
+  var j = musicians.length
+  for (let i=0;i < j ; i++) {
+    beatBand[i]= `${musicians[i]} plays ${instruments[i]}`
+  }
+  return beatBand
+}
+
+function johnLennonFacts(facts){
+  var johnfacts = new Array()
+  var i=0;
+  var j=facts.length
+  while (i<j){
+    johnfacts[i]=`${facts[i]}!!!`
+    i++
+  }
+
+  return johnfacts
+}
+
+function iLoveTheBeatles(i){
+  var iLove = new Array()
+  var j= -1
+  do {
+            iLove[j++]="I love the Beatles!"
+
+  }  while ( i++<= 15)
+  return iLove
+
+}
